b1cfa3632737708fd423272d11c169b364d45356
Soliman Samy <soliman1samy@gmail.com>
2019-01-25 08:28:08 +0200

    Add PostBack entity

---
+++ src/AppBundle/Entity/PostBack.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PostBack
+ *
+ * @ORM\Table(name="post_back")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PostBackRepository")
+ */
+class PostBack
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="json_array")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return PostBack
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
