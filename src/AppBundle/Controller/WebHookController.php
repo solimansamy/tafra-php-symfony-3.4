bc9e0c83350266422edf95abe5d124d6cabd1ceb
Soliman Samy <soliman1samy@gmail.com>
2019-01-26 02:07:19 +0200

    Get weather information

--- src/AppBundle/Controller/WebHookController.php
+++ src/AppBundle/Controller/WebHookController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/api/webhook")
@@ -27,9 +28,57 @@ class WebHookController extends Controller
      */
     public function WhatsappAction()
     {
-        return $this->render('AppBundle:WebHook:whatsapp.html.php', array(
-            // ...
-        ));
+        // listen to the POST request from Dialogflow
+        $request = file_get_contents("php://input");
+        $requestJson = json_decode($request, true);
+
+        $city = $requestJson['queryResult']['parameters']['geo-city'];
+
+        if (isset($city)) {
+            echo $this->getWeatherInformation($city);exit;
+        }
+    }
+
+    /**
+     * Makes an API call to OpenWeatherMap and
+     * retrieves the weather data of a given city.
+     *
+     * @param string $city
+     *
+     * @return void
+     */
+    private function getWeatherInformation($city)
+    {
+        $apiKey = '4960e8061ca1902d42f16f9313c6fbcf';
+        $weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey";
+        $weather = file_get_contents($weatherUrl);
+
+        $weatherDetails =json_decode($weather, true);
+
+
+        $temperature = round($weatherDetails["main"]["temp"]);
+        $weatherDescription = $weatherDetails["weather"][0]["description"];
+
+        return $this->sendFulfillmentResponse($temperature, $weatherDescription);
+    }
+
+    /**
+     * Send weather data response to Dialogflow
+     *
+     * @param integer $temperature
+     * @param string  $weatherDescription
+     *
+     * @return void
+     */
+    private function sendFulfillmentResponse($temperature, $weatherDescription)
+    {
+        $response = "It is $temperature degrees with $weatherDescription";
+
+        $fulfillment = array(
+            "fulfillmentText" => $response
+        );
+
+        return (json_encode($fulfillment));
     }
 
 }
